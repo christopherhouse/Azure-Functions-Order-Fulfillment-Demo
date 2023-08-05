44903085091636f84b9c29d2b7f02e177e60ed47
Chris House <chhouse@microsoft.com>
2023-08-05 14:30:38 -0500

    Pretty sure this will work.  yolo

--- .pipelines/scripts/Set-PipelineVariablesFromDeploymentOutput.ps1
+++ .pipelines/scripts/Set-PipelineVariablesFromDeploymentOutput.ps1
@@ -8,5 +8,8 @@ param(
 
 Write-Output "Deployment output is $armOutputString"
 $outputObj = $armOutputString | ConvertFrom-Json
+$functionAppName = $outputObj.functionAppName.value
 
-Write-Output "##vso[task.setvariable variable=functionAppName;]$outputObj.functionAppName"
+# Set variable with Function app name from deployment output.  This will be used
+# as input for the Function deploy step
+Write-Output "##vso[task.setvariable variable=functionAppName;]$functionAppName"
