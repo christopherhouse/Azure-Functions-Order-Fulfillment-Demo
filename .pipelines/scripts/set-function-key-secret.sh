191cae55c0fbce031f1d4df0301583a0e2343a5c
Chris House <chhouse@microsoft.com>
2023-08-06 17:46:18 -0500

    what?

--- .pipelines/scripts/set-function-key-secret.sh
+++ .pipelines/scripts/set-function-key-secret.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 KEY=$($key = az functionapp keys list --resource-group $0 --name $1 --query "functionKeys.default" -o tsv)
 
-az keyvault secret set --vault-name $2 --name "FunctionAppKey" --value "$KEY"
+az keyvault secret set --vault-name $2 --name "FunctionAppKey" --value $KEY
