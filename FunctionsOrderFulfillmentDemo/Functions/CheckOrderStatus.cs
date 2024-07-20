c8ccf2064e8fc5bdfe4defb64d3fbf16b3da6012
Chris House <chhouse@microsoft.com>
2024-07-20 10:31:48 -0500

    openapi spec update

--- FunctionsOrderFulfillmentDemo/Functions/CheckOrderStatus.cs
+++ FunctionsOrderFulfillmentDemo/Functions/CheckOrderStatus.cs
@@ -25,6 +25,8 @@ namespace FunctionsOrderFulfillmentDemo.Functions
         [OpenApiOperation(operationId: "Run", tags: new[] { "name" })]
         [OpenApiSecurity("function_key", SecuritySchemeType.ApiKey, Name = "code", In = OpenApiSecurityLocationType.Query)]
         [OpenApiParameter(name: "name", In = ParameterLocation.Query, Required = true, Type = typeof(string), Description = "The **Name** parameter")]
+        [OpenApiParameter(name: "customerId", In = ParameterLocation.Path, Required = true, Type = typeof(string), Description = "customerId")]
+        [OpenApiParameter(name: "orderId", In = ParameterLocation.Path, Required = true, Type = typeof(string), Description = "orderId")]
         [OpenApiResponseWithBody(statusCode: HttpStatusCode.OK, contentType: "text/plain", bodyType: typeof(string), Description = "The OK response")]
         public async Task<IActionResult> Run(
             [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = "orderStatus/{customerId}/{orderId}")] HttpRequest req,
